a9d6d4059db360d3b37c94d16bb4e164b4000d21
Екатерина <katya.borodich@list.ru>
2022-10-28 17:02:44 +0300

    Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,6 +6,27 @@ import java.util.ArrayList;
 
 public class Main {
 
+    public static void showAppliances(ArrayList<Applience> appliances) {
+        System.out.println("\nAll teapots:");
+        for (Applience appliance : appliances) {
+            System.out.println(appliance.toString());
+        }
+    }
+
+    public static void findLowestPrice(ArrayList<Applience> appliances) {
+        appliances.sort(new PriceComparator());
+        System.out.println("\nThe lowest price: " + appliances.get(0).toString());
+    }
+
+    public static void findAllTeapots(ArrayList<Applience> appliances) {
+        ArrayList<Applience> teapots = new ArrayList<>();
+        for (Applience appliance : appliances) {
+            if (appliance.getClass() == Teapot.class)
+                teapots.add(appliance);
+        }
+        showAppliances(teapots);
+    }
+
     public static void main(String[] args) {
         ArrayList<Applience> appliances = new ArrayList<>();
         appliances.add(new Teapot(85, 90, 1.2, 1.8, 30, "plastic"));
@@ -13,10 +34,9 @@ public class Main {
         appliances.add(new Fridge(221, 38, 20, 80, 170, 80, 350));
         appliances.add(new Laptop(1565, 2.2, 35, 50, 2, "plastic", 148, "Windows", 1000));
         appliances.add(new MobilePhone(1039, 0.8, 15, 7, 0.2, "metal", "Android", 128));
-        for (Applience appliance : appliances) {
-            System.out.println(appliance.toString());
-        }
-        appliances.sort(new PriceComparator());
-        System.out.println("The lowest price: " + appliances.get(0).toString());
+
+        showAppliances(appliances);
+        findLowestPrice(appliances);
+        findAllTeapots(appliances);
     }
 }
